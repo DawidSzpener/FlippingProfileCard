07534c637447e2352d813ae89a929f67fc04cf49
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-16 10:44:39 +0000

    Style left side of the card

--- src/components/FlippingProfileCard.js
+++ src/components/FlippingProfileCard.js
@@ -11,19 +11,19 @@ const FlippingCard = (props) => {
   if(isLeftSide){
     card = 
       <div className="flippingCard__left"  onClick={() => setIsLeftSide(!isLeftSide)}>
-        <img className="FlippingCard__left__background" alt="background" src={props.picture} />
-        <div className="FlippingCard__left__fullName">{props.fullName}</div>
-        <div className="FlippingCard__left__miniTitle">{props.miniTitle}</div>
+        <div className="flippingCard__left__background" style={{backgroundImage: `url(${props.picture})`}} />
+        <div className="flippingCard__left__fullName">{props.fullName}</div>
+        <div className="flippingCard__left__miniTitle">{props.miniTitle}</div>
       </div>
   } else {
     card = 
       <div className="flippingCard__right">
-        <button className="FlippingCard__right__arrow"  onClick={() => setIsLeftSide(!isLeftSide)}>{`<-`}</button>
-        <img className="FlippingCard__right__roundPicture" alt="background" src={props.picture} />
-        <div className="FlippingCard__right__fullName">{props.fullName}</div>
-        <div className="FlippingCard__right__miniTitle">{props.miniTitle}</div>
-        <div className="FlippingCard__right__description">{props.description}</div>
-        <button className="FlippingCard__right__button">Button</button>
+        <button className="flippingCard__right__arrow"  onClick={() => setIsLeftSide(!isLeftSide)}>{`<-`}</button>
+        <img className="flippingCard__right__roundPicture" alt="background" src={props.picture} />
+        <div className="flippingCard__right__fullName">{props.fullName}</div>
+        <div className="flippingCard__right__miniTitle">{props.miniTitle}</div>
+        <div className="flippingCard__right__description">{props.description}</div>
+        <button className="flippingCard__right__button">Button</button>
       </div>
   }
   
