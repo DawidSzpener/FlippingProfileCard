8a38ca064e11de17ddd2a356402bd3902e6c9699
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-17 13:03:16 +0000

    Add gifs, images for readme

--- README.md
+++ README.md
@@ -7,11 +7,19 @@
 ## INSTALLATION 
 
 - clonse this repo
-- extract ```FlippingCard.js``` and ```FlippingCard.scss``` to your own project
-- you need to have sass installed as one of your projects dependency
+- extract ```FlippingCard.js``` and ```FlippingCard.scss``` files to your own project
+- you need to have sass installed as one of your projects dependency ```npm install sass`
 
 ## USAGE
 
 Available props: 
 
-- 
+![demo]()
+
+- theme (black, blue, red, green)
+- the rest is self explanatory 
+
+![demo]()
+![demo]()
+![demo]()
+![demo]()
--- src/App.js
+++ src/App.js
@@ -8,25 +8,7 @@ function App() {
     <div className="App">
       <div style={{float: 'left'}}>
         <FlippingCard 
-          theme="black"
-          picture={img}
-          fullName="Dawid Szpener"
-          miniTitle="Software Engineer"
-          description="Jestem Dawid, od 2 lat ucze sie programowac, lubie gotowac i spedzac czas z Ola"
-        />
-      </div>
-      <div style={{float: 'left', marginLeft: '4%'}}>
-        <FlippingCard 
-          theme="blue"
-          picture={img}
-          fullName="Dawid Szpener"
-          miniTitle="Software Engineer"
-          description="Jestem Dawid, od 2 lat ucze sie programowac, lubie gotowac i spedzac czas z Ola"
-        />
-      </div>
-      <div style={{float: 'left', marginLeft: '4%'}}>
-        <FlippingCard 
-          theme="red"
+          theme="green"
           picture={img}
           fullName="Dawid Szpener"
           miniTitle="Software Engineer"
