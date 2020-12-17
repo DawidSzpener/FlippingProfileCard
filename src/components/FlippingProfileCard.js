8585246efb718b746b2e218195202c614e575a53
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-17 10:21:09 +0000

    Refaktor front/back side opacity

--- src/components/FlippingProfileCard.js
+++ src/components/FlippingProfileCard.js
@@ -8,13 +8,13 @@ const FlippingCard = (props) => {
   const [frontSideClass, setFrontSideClass] = useState(["flippingCard__front"])
   const [backSideCLass, setBackSideClass] = useState(["flippingCard__back"])
 
-  let frontSide = null
-  let backSide = null
-
   useEffect(() => {
     if(isFrontSide) {
       setFrontSideClass(["flippingCard__front", "flippingCard__front--entry-flip"])
       setBackSideClass(["flippingCard__back", "flippingCard__back--exit-flip"])
+    } else if (isFrontSide) {
+      setFrontSideClass(["flippingCard__front"])
+      setBackSideClass(["flippingCard__back"])
     } else {
       setBackSideClass(["flippingCard__back", "flippingCard__back--entry-flip"])
       setFrontSideClass(["flippingCard__front", "flippingCard__front--exit-flip"])
@@ -22,41 +22,34 @@ const FlippingCard = (props) => {
 
   }, [isFrontSide])
 
+  let frontSideOpacity = 0
+  let backSideOpacity = 0
+
   if(isFrontSide){
-    frontSide = 
-      <div className={frontSideClass.join(' ')}  onClick={() => setIsFrontSide(!isFrontSide)}>
-        <div className="flippingCard__front__radial-gradient"/>
-        <div className="flippingCard__front__background" style={{backgroundImage: `url(${props.picture})`}} />
-        <div className="flippingCard__front__fullName">{props.fullName}</div>
-        <div className="flippingCard__front__miniTitle">{props.miniTitle}</div>
-      </div>
-    backSide =
-      <div className={backSideCLass.join(' ')} style={{opacity: '0'}}>
-        <button className="flippingCard__back__arrow"  onClick={() => setIsFrontSide(!isFrontSide)}>⬅</button>
-        <img className="flippingCard__back__roundPicture" alt="background" src={props.picture} />
-        <div className="flippingCard__back__fullName">{props.fullName}</div>
-        <div className="flippingCard__back__miniTitle">{props.miniTitle}</div>
-        <div className="flippingCard__back__description">{props.description}</div>
-        <button className="flippingCard__back__button">Follow</button>
-      </div>
+    frontSideOpacity = 1
+    backSideOpacity = 0
   } else {
-    frontSide = 
-      <div className={frontSideClass.join(' ')} style={{opacity: '0'}} onClick={() => setIsFrontSide(!isFrontSide)}>
-        <div className="flippingCard__front__radial-gradient"/>
-        <div className="flippingCard__front__background" style={{backgroundImage: `url(${props.picture})`}} />
-        <div className="flippingCard__front__fullName">{props.fullName}</div>
-        <div className="flippingCard__front__miniTitle">{props.miniTitle}</div>
-      </div>
-    backSide =
-      <div className={backSideCLass.join(' ')}>
-        <button className="flippingCard__back__arrow"  onClick={() => setIsFrontSide(!isFrontSide)}>⬅</button>
-        <img className="flippingCard__back__roundPicture" alt="background" src={props.picture} />
-        <div className="flippingCard__back__fullName">{props.fullName}</div>
-        <div className="flippingCard__back__miniTitle">{props.miniTitle}</div>
-        <div className="flippingCard__back__description">{props.description}</div>
-        <button className="flippingCard__back__button">Follow</button>
-      </div>
+    frontSideOpacity = 0
+    backSideOpacity = 1
   }
+
+  let frontSide = 
+  <div className={frontSideClass.join(' ')} style={{opacity: frontSideOpacity}}  onClick={() => setIsFrontSide(!isFrontSide)}>
+    <div className="flippingCard__front__radial-gradient"/>
+    <div className="flippingCard__front__background" style={{backgroundImage: `url(${props.picture})`}} />
+    <div className="flippingCard__front__fullName">{props.fullName}</div>
+    <div className="flippingCard__front__miniTitle">{props.miniTitle}</div>
+  </div>
+  
+  let backSide = 
+    <div className={backSideCLass.join(' ')} style={{opacity: backSideOpacity}}>
+      <button className="flippingCard__back__arrow"  onClick={() => setIsFrontSide(!isFrontSide)}>⬅</button>
+      <img className="flippingCard__back__roundPicture" alt="background" src={props.picture} />
+      <div className="flippingCard__back__fullName">{props.fullName}</div>
+      <div className="flippingCard__back__miniTitle">{props.miniTitle}</div>
+      <div className="flippingCard__back__description">{props.description}</div>
+      <button className="flippingCard__back__button">Follow</button>
+    </div>
   
   return (
     <div className="flippingCard">
